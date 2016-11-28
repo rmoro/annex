b0a6dcc3b577104ef71c7ae4f87f3092cdd46290
rmoro <moro1422@mylaurier.ca>
2016-11-28 01:41:14 -0500

    bad virtual key codes :(

--- import_scripts/dsl_importer.py
+++ import_scripts/dsl_importer.py
@@ -3,13 +3,18 @@
 # EMAIL:    robert@morouney.com 
 # FILE:     csv-key.py
 # CREATED:  2016-11-27 17:44:33
-# MODIFIED: 2016-11-27 17:57:41
+# MODIFIED: 2016-11-28 01:37:24
 #////////////////////////////////////////////////////////////////////////////////
 
+password = [47,17,34,14,23,56,15,31,0,45,36]
 
 with open('dsl.csv', 'r') as fin:
     for line in fin: 
         vals = line.split(',')
-        with open('./logs/'+vals[0], 'a') as fout:
-            for i,c in enumerate(".tie5Roanl"):
-                print("{},{}".format(ord(c),int((float(vals[3+(i*3)])+float(vals[4+(i*3)]))*1000)), file=fout)
+        with open('../dsl_logs/'+vals[0], 'a') as fout:
+            for i,c in enumerate(password):
+                idx = 3 * (i+1)
+                dwell = float(vals[idx])
+                flight = float(vals[idx+1])
+                dt = int( (dwell + flight)*1000 )
+                print("{},{}".format(dt, c), file=fout)
